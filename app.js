996a0ea6cd36601a78fa2abf4f0b35fca419ee06
hrofarrow <hrofarrow@gmail.com>
2020-10-14 11:10:18 -0400

    Completed dropdown event for bar graph

--- app.js
+++ app.js
@@ -5,26 +5,25 @@
 // });
 
 // initialize the page with a default plot 
-function init() {
     var oneHundreds = [];
     var twoHundreds = [];
     var threeHundreds = [];
     var fourHundreds = [];
     var fiveHundreds = [];
     var sixHundreds = [];
-    var sevenHundreds = [];
     var eightHundreds = [];
-    var nineHundreds = [];
-    var thousands = [];
+function init() {
     
-    var groups = ["100's", "200's", "300's", "400's", "500's", "600's", "700's", "800's", "900's", "1000's"]
+    var groups = ["100's", "200's", "300's", "400's", "500's", "600's", "800's"]
     
     // read in the data 
     d3.csv("new_names.csv").then(function(data) {
         console.log(data);
+        // convert the column from string to integer
+        data.forEach(function(d){ d['propertyclasscode'] = +d['propertyclasscode']});
     
             for (var i = 0; i < data.length; i++) {
-                var code = data[i].property_class_code;
+                var code = data[i].propertyclasscode;
         
                 if(code >= 100 && code < 200){
                     oneHundreds.push(code);
@@ -44,25 +43,15 @@ function init() {
                 if(code >= 600 && code < 700) {
                     sixHundreds.push(code);
                 }
-                if(code >= 700 && code < 800) {
-                    sevenHundreds.push(code);
-                }
                 if(code >= 800 && code < 900) {
                     eightHundreds.push(code);
                 }
-                if(code >=900 && code < 1000) {
-                    nineHundreds.push(code);
-                }
-                if(code >=1000) {
-                    thousands.push(code);
-                }
-                // else 
-                // console.log(code);
         
               }
+
         var bars = [oneHundreds.length, twoHundreds.length, threeHundreds.length, fourHundreds.length, fiveHundreds.length, 
-                sixHundreds.length, sevenHundreds.length, eightHundreds.length, nineHundreds.length, thousands.length];
-    
+                sixHundreds.length, eightHundreds.length];
+                
         var trace1 = {
             type: "bar",
             x: groups,
@@ -103,5 +92,68 @@ function init() {
         Plotly.newPlot("bar", plotData, layout);
     });
 }
-init();
 
+// get unique values of each array 
+
+function removeDuplicates(array) {
+    var unique = []
+    array.map(x => {
+        if (!unique.includes(x)) {
+        (unique.push(x));
+      }
+    })
+    console.log(unique)
+  };
+  removeDuplicates(fiveHundreds)
+
+// function to change plot based on dropdown choice 
+d3.selectAll("#selDataset").on("change", updatePlotly);
+
+// function to update the graph via the dropwdown 
+function updatePlotly() {
+    // use d3 to select the dropdown menu 
+    var dropdownMenu = d3.select("#selDataset");
+    // assign the value of the dropdown menu to variable 
+    var dataset = dropdownMenu.property("value");
+
+    // initialize the arrays to be used 
+    var x = []
+    var y = []
+
+    if (dataset === "one") {
+        x = [100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110];
+        y = oneHundreds.count;
+    }
+
+    if (dataset === "two") {
+        x = [200]
+        y = twoHundreds.count;
+    }
+    if (dataset === "three") {
+        x = [320, 330, 345, 350, 360, 370, 380, 385, 390, 398, 399]
+        y = threeHundreds.count;
+    }
+    if (dataset === "four") {
+        x = [419, 422, 425, 429, 431, 442, 444, 447, 454, 480, 481, 499];
+        y = fourHundreds.count;
+    }
+    if (dataset === "five") {
+        x = [500, 501, 505, 510, 511, 520, 521, 530, 540, 550, 599];
+        y = fiveHundreds.count;
+    }
+    if (dataset === "six") {
+        x = [600, 610, 620, 630, 640, 650, 660, 670, 680, 686];
+        y = sixHundreds.count;
+    }
+    if (dataset === "eight") {
+        x = [800, 805, 810, 815, 820, 830, 840, 850, 860, 870, 875];
+        y = eightHundreds.count;
+    }
+
+    // restyle the graph based on the dropdown selection 
+    Plotly.restyle("bar", "x", [x]);
+    Plotly.restyle("bar", "y", [y]);
+
+
+    }
+init();
--- update_csv.py
+++ update_csv.py
@@ -1,10 +1,16 @@
-import pandas
+import pandas as pd
 
-df = pandas.read_csv("county49Merged.csv")
+data = pd.read_csv("county49Merged.csv")
 
-df = df.rename(columns=({"ZIP Code": "zip_code", "Property Class Code": "property_class_code", "Neighborhood ID": "neighborhood_id",
-                         "Date Last Transferred": "date_transferred", "Total Adj Value": "total_property_value", "Owner State": "owner_state", "Owner ZIP Code": "owner_zip"}))
+df = pd.DataFrame(data)
 
-# print(df)
+# convert column from string to integer
+df[["propertyclasscode"]] = df[[
+    "propertyclasscode"]].apply(pd.to_numeric)
 
-df.to_csv("new_names.csv", index=False)
+new_df = df.rename(columns=({"ZIP Code": "zip_code", "Property Class Code": "property_class_code", "Neighborhood ID": "neighborhood_id",
+                             "Date Last Transferred": "date_transferred", "Total Adj Value": "total_property_value", "Owner State": "owner_state", "Owner ZIP Code": "owner_zip"}))
+
+print(new_df.propertyclasscode.unique())
+
+new_df.to_csv("new_names.csv", index=False)
